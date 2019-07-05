96bf01319d7aac496a86577d34cba913f432aec9
yeafel@qq.com <kyf19960629>
2019-07-05 18:03:34 +0800

    测试jedis

--- src/main/java/JedisDemo1.java
+++ src/main/java/JedisDemo1.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author Yeafel
@@ -26,6 +28,52 @@ public class JedisDemo1 {
 
         //4、释放资源
         jedis.close();
+
+    }
+
+    /**
+     * 连接池方式连接
+     */
+    @Test
+    public void demo2() {
+        // 获得连接池的配置对象
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+
+        //设置最大连接数
+        jedisPoolConfig.setMaxTotal(30);
+
+        //设置最大空闲连接数
+        jedisPoolConfig.setMaxIdle(30);
+
+        //获得连接池
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig,"139.9.251.246",6379);
+
+        //获得核心对象
+        Jedis jedis = null;
+
+        try {
+            //通过连接池来获得连接
+            jedis = jedisPool.getResource();
+
+            //设置数据
+            jedis.set("name","张三");
+
+            //获取数据
+            String value = jedis.get("name");
+            System.out.println(value);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            //释放资源
+            if (jedis != null) {
+                jedis.close();
+            }
+            if (jedisPool != null) {
+                jedis.close();
+            }
+        }
+
 
     }
 }
